cc5df0dc7fcf27d5fdefdd9cfda715d3b6b36649
Sandeep Raj Saimon <sandeeprajsaimon999@gmail.com>
2020-02-05 16:52:30 +0530

    third commit -rename

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 # pages/views.py
-from django.http import HttpResponse
-def homePageView(request):
-	return HttpResponse('Hello, World!')
+from django.views.generic import TemplateView
+class HomePageView(TemplateView):
+	template_name = 'home.html'
+class AboutPageView(TemplateView):
+	template_name='about.html'
--- resume/urls.py
+++ resume/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# helloworld_project/urls.py
+# pages_project/urls.py
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # new
 urlpatterns = [
 path('admin/', admin.site.urls),
-path('', include('pages.urls')),
+path('', include('pages.urls')), # new
 ]

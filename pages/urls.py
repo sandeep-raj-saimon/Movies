706a8665cb49a09584716585a90d71af73b31279
Sandeep Raj Saimon <sandeeprajsaimon999@gmail.com>
2020-02-12 17:43:45 +0530

    2nd Feb Evening

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,13 @@
 # pages/urls.py
 from django.urls import path
-from .views import HomePageView,AboutPageView
+from .views import HomePageView,AboutPageView,LoginPageView,RegisterPageView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 path('about/', AboutPageView.as_view(), name='about'), # new
-path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+path('login/', LoginPageView.as_view(), name='login'),
+path('regsiter/', RegisterPageView.as_view(), name='register'), # new
+#path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 path('', HomePageView.as_view(), name='home'),
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,12 @@
 # pages/views.py
 from django.views.generic import TemplateView
-from django.urls import path, include
+from django.urls import path, include,reverse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from pages.forms import UserForm
 
- 
 """class HomePageView(TemplateView):
 	template_name = 'home.html'"""
 
@@ -18,6 +20,65 @@ class AboutPageView(TemplateView):
 		names = {"myname":"sandeep"}
 		return render(request, "about.html",names)
 
-"""class LoginPageView(TemplateView):
-	template_name='login.html'""" 
+class LoginPageView(TemplateView):
+	def get(self, request, *args, **kwargs):
+		return render(request,"login.html")
 	
+	#new
+	def post(self,request,*args,**kwargs):
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+			user = authenticate(username=username, password=password)
+			if user:
+				if user.is_active:
+					login(request,user)
+					return HttpResponseRedirect(reverse('home'))
+				else:
+					return HttpResponse("Your account was inactive.")
+			else:
+				print("Someone tried to login and failed.")
+				print("They used username: {} and password: {}".format(username,password))
+				return HttpResponse("Invalid login details given")
+		else:
+			return render(request, 'login.html', {})
+			
+			
+class RegisterPageView(TemplateView):
+	
+	#new
+	def get(self, request, *args, **kwargs):
+		registered=False
+		user_form = UserForm()
+		
+		return render(request,'register.html',{'user_form':user_form,'registered':registered})
+	
+		
+	def post(self,request,*args,**kwargs):
+		registered = False
+		if request.method == 'POST':
+			user_form = UserForm(data=request.POST)
+			
+			if user_form.is_valid():
+				user = user_form.save()
+				user.set_password(user.password)
+				user.save()
+				registered = True
+			else:
+				print(user_form.errors)
+		else:
+			user_form = UserForm()
+		
+		return render(request,'register.html',{'user_form':user_form,'registered':registered})
+
+#new
+
+@login_required
+def special(request):
+    return HttpResponse("You are logged in !")
+	
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
+
